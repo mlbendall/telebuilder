0735a0e7f7a13b8cd369a1617320facb3e153cdb
Matthew Bendall <bendall@gwu.edu>
2017-09-25 17:13:02 -0400

    first working version

---
+++ setup.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+__author__ = 'Matthew L. Bendall'
+__copyright__ = "Copyright (C) 2017 Matthew L. Bendall"
+
+setup(
+    name='telebuilder',
+    version='0.1',
+    description='Identify transposable element loci from repeat annotations',
+    url='http://github.com/mlbendall/telebuilder',
+    author='Matthew L. Bendall',
+    author_email='bendall@gwu.edu',
+    license='MIT',
+    packages=['telebuilder'],
+    install_requires=[
+        'intervaltree',
+    ],
+    entry_points={
+        'console_scripts': [
+            'buildERV=telebuilder.buildERV:console',
+            'analyzeERV=telebuilder.analyzeERV:console',
+            'gtftools=telebuilder.gtftools:console',
+        ],
+    },
+    zip_safe=False,
+)
+
