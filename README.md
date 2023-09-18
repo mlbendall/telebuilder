178a9c07beadac9d60fe785666b16e16010b3b1b
Matthew L. Bendall <mlb4001@med.cornell.edu>
2023-09-18 14:23:17 -0700

    added versioneer

---
+++ README.md
@@ -0,0 +1,10 @@
+# Telebuilder
+
+## Installing
+
+```bash
+mamba create -n telebuilder_dev python=2.7 biopython intervaltree
+conda activate telebuilder_dev
+pip install -e .
+```
+
--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
 # -*- coding: utf-8 -*-
 
 from setuptools import setup, find_packages
+import versioneer
 
 __author__ = 'Matthew L. Bendall'
-__copyright__ = "Copyright (C) 2017 Matthew L. Bendall"
+__copyright__ = "Copyright (C) 2023 Matthew L. Bendall"
 
 setup(
     name='telebuilder',
-    version='0.1',
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
     description='Identify transposable element loci from repeat annotations',
     url='http://github.com/mlbendall/telebuilder',
     author='Matthew L. Bendall',
